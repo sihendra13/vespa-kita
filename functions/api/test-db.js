4b24f77a06db7227a19a8cd5a6a1d39d0e079ad9
Hendra Fitriadi <kontesku1374@gmail.com>
2026-09-02 11:39:10 +0700

    chore: test db updates

--- functions/api/test-db.js
+++ functions/api/test-db.js
@@ -1,4 +1,8 @@
 export async function onRequestGet({ env }) {
-  const { results } = await env.DB.prepare(`SELECT author_name, google_sub FROM tongkrongan_posts ORDER BY created_at DESC LIMIT 10`).all();
-  return new Response(JSON.stringify(results), { headers: { "content-type": "application/json" } });
+  const { results } = await env.DB.prepare(`
+    SELECT * FROM tongkrongan_posts 
+    WHERE content LIKE '%Desember Rabu%' OR author_name = 'Desember Rabu'
+    ORDER BY created_at DESC LIMIT 10
+  `).all();
+  return new Response(JSON.stringify(results, null, 2), { headers: { "content-type": "application/json" } });
 }
